457f82436be98feef5c682a663f2342c84030fde
Ed Wilde <git@edwilde.com>
2012-08-21 10:56:29 +0100

    Use AppleWidget.js
    Notify the AppleWidget.js class within iBooks that content is ready to show - this removes the 'flash' when a widget is loaded for the first time.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-// Put your code here
+// put your code here
+
+
+(function($){
+	$(document).ready(function(){
+		/* code to run when widget is loaded */
+
+		// tell apple class that content is ready for viewing
+		widget.notifyContentIsReady(); 
+	});
+})(jQuery);
+
